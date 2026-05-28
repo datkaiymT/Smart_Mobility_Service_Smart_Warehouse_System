70fa8c65f5c3fbcd98653b9c0bd30cfd1bbcfb7c
DatkaiymT <datkaiymt27@gmail.com>
2026-05-28 17:06:01 +0900

    Build: Reset main branch to a clean canvas

--- my_project/scripts/hello.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import rospy
-
-if __name__ == '__main__':
-    rospy.init_node('hello_node')
-    rate = rospy.Rate(1)
-    while not rospy.is_shutdown():
-        rospy.loginfo("Project started successfully!")
-        rate.sleep()
